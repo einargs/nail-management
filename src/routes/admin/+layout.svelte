6cc85687e1fb7d70ab6c88c8385382ea89ad32ff
Einar Strandberg <einargs@gmail.com>
2025-11-24 16:35:38 -0600

    added icons

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -15,20 +15,20 @@
 {/snippet}
 
 <NavigationMenu.Root class="p-5 shadow-md w-full max-w-full flex flex-row justify-start bg-primary text-primary-foreground">
-  <NavigationMenu.List class="items-start group flex flex-row justify-start gap-2">
-  {@render PageLink("Main", "/")}
-  {#if data.user}
-    <NavigationMenu.Item>
-      <NavigationMenu.Link class="text-lg font-medium hover:bg-accent">
-      <form method="POST" action="/login?/logout">
-        <button>Logout</button>
-      </form>
-      </NavigationMenu.Link>
-    </NavigationMenu.Item>
-  {:else}
-    {@render PageLink("Login", "/login")}
-  {/if}
-  <div class="text-lg font-medium">{data.user?.username ?? "no user"}</div>
+  <NavigationMenu.List class="items-start group flex flex-row justify-start gap-2 items-center">
+    {@render PageLink("Main", "/")}
+    {@render PageLink("Admin", "/admin")}
+    {#if data.user}
+      <NavigationMenu.Item>
+        <NavigationMenu.Link class="text-lg font-medium hover:bg-accent">
+        <form method="POST" action="/admin/login?/logout">
+          <button>Logout</button>
+        </form>
+        </NavigationMenu.Link>
+      </NavigationMenu.Item>
+    {:else}
+      {@render PageLink("Login", "/admin/login")}
+    {/if}
   <!-- 
     <NavigationMenu.Item>
       <NavigationMenu.Trigger>Item One</NavigationMenu.Trigger>
@@ -39,6 +39,11 @@
     <Skeleton class="size-12 rounded-full" />
 -->
   </NavigationMenu.List>
+  <div class="grow-1"></div>
+  <div class="flex-row flex gap-2 items-center">
+    <span class="block icon-[material-symbols--person] text-4xl"></span>
+    <div class="text-lg font-medium">{data.user?.username ?? "no user"}</div>
+  </div>
 </NavigationMenu.Root>
 
 <div class="flex flex-col items-center w-screen">
